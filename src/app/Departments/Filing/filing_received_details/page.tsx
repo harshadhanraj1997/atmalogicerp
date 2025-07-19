5fb0e268e8a55d22bb39d20e66c57cb26b6472ba
charanvadhyar <142227706+charanvadhyar@users.noreply.github.com>
2025-07-19 14:10:46 +0530

    Update page.tsx

--- src/app/Departments/Filing/filing_received_details/page.tsx
+++ src/app/Departments/Filing/filing_received_details/page.tsx
@@ -8,7 +8,7 @@ import { z } from 'zod';
 import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
 import { Label } from "@/components/ui/label";
 
-const apiBaseUrl = "https://needha-erp-server-xrdp.onrender.com";
+const apiBaseUrl = "https://needha-erp-server-jmjf.onrender.com";
 
 interface Details {
   Name: string;
